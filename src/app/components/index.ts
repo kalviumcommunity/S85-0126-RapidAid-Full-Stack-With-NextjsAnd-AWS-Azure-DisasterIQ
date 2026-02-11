cf40b7f9f573c5e2af73b36905c388ff77c83d34
PranavX500 <ps5840432@gmail.com>
2026-02-11 16:56:23 +0530

    Add barrel exports for components to fix case-sensitive build issues

---
+++ src/app/components/index.ts
@@ -0,0 +1,7 @@
+export { DashboardLayout } from "./DashboardLayout";
+export { AuthInput } from "./AuthInput";
+export { default as ComingSoon } from "./ComingSoon";
+export { default as CreateDisasterForm } from "./CreateDisasterForm";
+export { DisasterCard } from "./DisasterCart";
+export { StatCard } from "./StatCart";
+export { StatusBadge } from "./StatusBadge";
---
+++ src/app/components/ui/index.ts
@@ -0,0 +1,8 @@
+export { Button } from "./button";
+export { Input } from "./input";
+export { Label } from "./label";
+export { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./select";
+export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./card";
+export { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./dialog";
+export { Badge } from "./badge";
+export { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs";
